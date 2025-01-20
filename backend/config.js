5800dd7517b97da85d4a617cf303d10f0e359b45
hungblqn <77875957+hungblqn@users.noreply.github.com>
2025-01-20 16:19:08 +0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@
 export const PORT = 8080;
 //
 //export const FEAddress = 'http://localhost'
-export const BEAddress = 'https://realtime-backend-seven.vercel.app'
+export const BEAddress = 'https://7bfd7c10-6a3c-4e91-9eae-be3740806fc5-00-281sbrzv3tatz.sisko.replit.dev/'
 export const FEAddress = 'https://realtime-beige.vercel.app'
 
 export const jwtSecretKey = 'vQYUZa2HXqj4Nf5r8LyhTSvBEGz6puJW'
