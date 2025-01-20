b88fafadaaf6954b7d51fdcefb835b56972fb98e
hungblqn <77875957+hungblqn@users.noreply.github.com>
2025-01-20 16:13:58 +0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // Địa chỉ backend
-export const ServerAddress = 'https://realtime-backend-seven.vercel.app';
+export const ServerAddress = 'https://7bfd7c10-6a3c-4e91-9eae-be3740806fc5-00-281sbrzv3tatz.sisko.replit.dev';
